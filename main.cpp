374b7f33701b6f9fe2cbf1431af015573007c359
ryang <ryangae7@gmail.com>
2024-10-15 18:24:10 -0400

    Update to parser (10/15/2024)

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <stdio.h>
+#include <sstream>
 #include "rules.cpp"
 
 int main()
 {
-    
+    std::string input = "Hello world 101 != 50";
+    parse(input);
     return 0; 
 }
--- rules.cpp
+++ rules.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <map>
 #include <cctype>
+#include <vector> 
+#include <sstream>
 
 // Create reserved tokens for the parser
 enum reservedToken
@@ -27,7 +29,7 @@ enum reservedToken
 
 
 // Map strings to tokens (for the keywords and operator)
-reservedToken getToken(const std::string& str)
+reservedToken getToken(const std::string &str)
 {
     std::map<std::string, reservedToken> keywordOperatorMap = {
         {"return", RETURN},
@@ -62,5 +64,67 @@ reservedToken getToken(const std::string& str)
     if(std::isalpha(str[0]))
     {
         return IDENTIFIER;
+    }
+
+    // Otherwise, following input is invalid
+    return INVALID;
+}
+
+
+// Vectorize and tokenize the input 
+std::vector<std::string> tokenize(std::string &input)
+{
+    // Gather stream of char/strings via input
+    std::stringstream stream (input);
+    std::vector<std::string> tokens; std::string token;
+    while(stream >> token)
+        tokens.push_back(token);
+    return tokens;
+}
+
+
+static void parse (std::string& input)
+{
+    auto tokens = tokenize(input);
+
+    // Operate on different cases
+    for(std::string &tokenizedString : tokens){
+        reservedToken rToken = getToken(tokenizedString);
+        
+        // Different cases
+        switch(rToken){
+            case RETURN: 
+                std::cout << "Return statement found, exit" << std::endl;
+                break;
+            case WHILE:
+                std::cout << "While loop found, loop" << std::endl;
+                break;
+            case IF:
+                std::cout << "If statement found, evaluate conditional" << std::endl;
+                break;
+            case VAR:
+                std::cout << "Variable declaration made!" << std::endl;
+                break;
+            case NUMCONST:
+                std::cout << "Number constant identified: " << rToken << std::endl;
+                break;
+            case IDENTIFIER:
+                std::cout << "Identifier found: " << rToken << std::endl;
+                break;
+            case EQ:
+                std::cout << "Logical equivalent found ==" << std::endl;
+                break;
+            case OR:
+                std::cout << "Logical OR found ||" << std::endl;
+                break;
+            case AND:
+                std::cout << "Logical AND found &&" << std::endl;
+                break;
+            case INVALID: // indicate failure
+                default:
+                    std::cout << "Invalid token: " << rToken << std::endl;
+                    break;
+
+        }
     }
 }
